ab9330111e3c8e5e4ef6a2637cb4ed1826c06aac
Nick Jacob <nicholas_jacob@brown.edu>
2012-05-02 03:20:17 -0400

    added code for historical range

--- exprfuncs.py
+++ exprfuncs.py
@@ -1,22 +1,13 @@
 #
 # Expression Functions
 ##
-from matplotlib.finance import quotes_historical_yahoo
-import datetime
+import datetime,yfinance
 
 def expr_test_b(x,d):
 	return x*((d%2)+1) # starts with 5 
 def expr_test_a(x,d):
 	return x # starts with 10
 
-def stockquote(tick,d):
-	""" returns the avg price on a given day """
-	return quotes_historical_yahoo(tick,d,d)[0][-2] # makes a lot of unsafe assumptions
-
-"""
-1. price -- done (stockquote)
-2. PE 
-3. div yield
-4. YTD
-5. beta
-""" 
+def historical_price(ticker,date):
+	""" takes a datetime.date and a string ticker"""
+	return yfinance.get_historical(ticker,date)
--- triggerfuncs.py
+++ triggerfuncs.py
@@ -1,6 +1,6 @@
 # trigger functions
 ##
-
+import yfinance
 
 def test_oncall():
 	print 'trigger called'
@@ -8,3 +8,24 @@ def test_oncall():
 
 def test_getPrice(data):
 	return 1 # price stays constant
+
+
+def buy_stock(ticker,amount,type):
+	"""
+	ticker: stock ticker
+	amount: quantity
+	type: 'SHARES' or 'DOLLARS'
+	"""
+	value = yfinance.get_price(ticker)
+	if type == 'SHARES': return (amount,value)
+	amount //= value # you can only buy full shares of stock!
+	return (amount,value)
+
+def sell_stock(ticker,amount,type):
+	"""
+	ticker: stock symbol
+	amount: quantity
+	type: 'SHARES' or 'DOLLARS'
+	"""
+	value = yfinance.get_price(ticker)
+	
